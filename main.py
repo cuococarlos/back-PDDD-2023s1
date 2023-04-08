f097fdd386d61f11776d3a9c604c3d641a816f4c
charlie@camba.coop <charlie@camba.coop>
2023-04-08 20:34:19 -0300

    agregado pymongo

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 load_dotenv()
 app = Flask(__name__)
+
+client = MongoClient(
+    "localhost",
+    int(os.getenv("MONGODB_PORT", 27017)),
+    username="root",
+    password="123456",
+)
 
 
 @app.route("/")
